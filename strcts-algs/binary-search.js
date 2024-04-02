ca5e7fbc1763895dbcd61d1dcc59dea18f3ce0e6
NoahV17 <nvario1@gmail.com>
2024-04-02 16:00:56 -0400

    delete repeated code

--- strcts-algs/binary-search.js
+++ strcts-algs/binary-search.js
@@ -1,22 +1,3 @@
-function binarySearch(array, target) {
-    let left = 0;
-    let right = array.length - 1;
-
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-
-        if (array[mid] === target) {
-            return mid;
-        } else if (array[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return -1;
-}
-
 function binarySearchTasks(tasks, targetId) {
     let left = 0;
     let right = tasks.length - 1;
